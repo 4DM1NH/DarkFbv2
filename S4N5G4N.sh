9290ec9e80dab8d2f704d43dbd7791ba1a056d40
4DM1NH <66303775+4DM1NH@users.noreply.github.com>
2020-06-02 20:14:36 +0700

    Add files via upload

---
+++ S4N5G4N.sh
@@ -0,0 +1,13 @@
+#!/system/bin/sh
+
+echo ""
+echo "Baru Belajar Buat Script Gan"
+echo ""
+echo "Maklumi lah gan gw masih dalam proses belajar"
+echo ""
+echo "Ga suka"
+echo ""
+echo "Berantem Hayyuuk"
+echo ""
+echo "Awowkwowk Canda :v"
+sleep 3
